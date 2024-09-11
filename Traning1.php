2bd571e21e18fcf4fdff1458d8cbe133565e5cf3
VuyoYapi <103922433+VuyoYapi@users.noreply.github.com>
2024-09-11 13:25:40 +0200

    Add files via upload

---
+++ Traning1.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+	  <style>
+    .image_4 img {
+      display: block;
+      margin: 0 auto;
+      max-width: 100%;
+    }
+	 .doctor_section {
+      background-color: #f0f0f0; /* Example background color */
+      padding: 20px; /* Adjust padding as needed */
+    }
+    .image_4 img {
+      display: block;
+      margin: 0 auto;
+      max-width: 100%;
+    }
+	.vertical{
+		border-left:
+		6px solid black;
+		height: 200px;
+		position:absolute;
+		left: 50%;
+	}
+	.horizontal_line {
+            width: 90%;
+            height: 5px;
+            
+			border-bottom: 5px dotted black;
+            line-height: 80%;
+        }
+	</style>
+      <title>Bee Hive Care Training</title>
+	  <link rel="icon" href="Images/logo12-removebg-preview.png" width="100px" height="145px">
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+    </head>
+    <body>
+      <!-- header section start -->
+      <div class="header_section">
+        <nav class="destop_header navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"></div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Traning1.php">Training</a>
+              </li>
+              <li class="nav-item">
+                <a class="logo_main" href="index.php"><img src="images/logo12-removebg-preview.png"></a>
+              </li>
+			  
+              <li class="nav-item">
+                <a class="nav-link" href="Wishlist.php">Wishlist</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Incomer-Generator.php">Income-Generater</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="Gallery.php">Gallery</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="Donations.php">Donations</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src="images/search-icon.png"></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="LoginPage.php">LOGIN</a>
+              </li>
+<li class="nav-item active">
+                <a class="nav-link" href="https://www.tshupehospice.co.za/payfastdonate.html">Donate</a>
+              </li>               
+            </ul>
+          </div>
+        </nav>
+        <nav class="mobile_header navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Training1.php">Training</a>
+              </li>
+              <li class="nav-item">
+                <a class="logo_main" href="index.php"><img src="images/logo.png"></a>
+             <li class="nav-item">
+                <a class="nav-link" href="Wishlist.php">Wishlist</a>
+              </li>
+             <li class="nav-item">
+                <a class="nav-link" href="Incomer-Generator.php">Income-Generater</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="Gallery.php">Gallery</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="Donations.php">Donations</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src="images/search-icon.png"></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="LoginPage.php">LOGIN</a>
+              </li>              
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <!-- header section end -->
+      <!-- doctor section start -->
+      <div class="doctor_section layout_padding">
+        <div class="container">
+		<h1 class="highest_text">Training</h1>
+          <div class="row">
+            <div class="col-md-6 padding_top0">
+              <h2 class="about_text">WE OFFER THE FOLLOWING TRAINING:</h2>
+                <h3>QCTO Accredited NQF Level 3 Home Based Personal Carer Qualification (135 credits)</h3>
+                <p class="lorem_text">12 Months Full-Time Training; Practicals and Work Skills Placement Training.<br>
+
+
+Matric with English and Maths (Core or Lit.)<br>
+
+
+Applications for our accredited courses close on 01 March 2022 
+ <div class="horizontal_line"></div></p>
+                
+				
+                <h3>Our Credentials & Accreditation</h3> <p class="lorem_text">
+				- 1 star accreditation with Council for Health Services Accreditation of Southren Africa (COHSASA)<br>
+- Registered member of Hospice Palliative Care Association.<br>
+- NPO Registration: (034-695-NPO)<br>
+- Income Tax Exemption Approval (Section 18A)<br>
+-QCTO accreditation number:SDP1227/20/00101
+				<div class="horizontal_line"></div></p>	
+           <div class="read_main">
+                  <div class="read_bt"><a href="#">Read More</a></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+              <div class="image_4"><img src="images/istockphoto-165558246-612x612-removebg-preview.png"></div>
+            </div>
+			<div class="col-md-6">
+              <div class="image_4"><img src="images/tshupetraining.jpg"></div>
+            </div>
+			<div class="col-md-6">
+              <div class="image_4"><img src="images/TshupeTraining1.png"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- doctor section end -->
+      <!-- footer section start -->
+      <!-- info section -->
+ <div class="info_section layout_padding">
+      <div class="container ">
+        <div class="info_content">
+              <div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Child Care
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                 
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Income-Generator
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+                  
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                 
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Training
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Gallery.php">
+                      Gallery
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center align-items-lg-baseline">
+          <div class="social-box">
+            <a href="">
+              <img src="images/fb-icon.png" alt="" />
+            </a>
+
+            <a href="">
+              <img src="images/twitter-icon.png" alt="" />
+            </a>
+            <a href="">
+              <img src="images/linkedin-icon.png" alt="" />
+            </a>
+            <a href="">
+              <img src="images/instagram-icon.png" alt="" />
+            </a>
+          </div>
+          <div class="form_container mt-5">
+            <form action="">
+              <label for="subscribeMail">
+                Newsletter
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Your email"
+                id="subscribeMail"
+              />
+              <button type="submit">
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <!-- end info section -->
+    <!-- footer section end -->
+    <!-- copyright section start -->
+    <div class="copyright_section">
+      <div class="container">
+        <p class="copyright">2024 All Rights Reserved. Design by <a href="https://html.design">Vuyo Yapi Designs</a></p>
+      </div>
+    </div>
+    <!-- copyright section end -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+	  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+   </body>
+   </html>
