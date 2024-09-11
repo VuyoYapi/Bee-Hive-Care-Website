cf8875929b1301c416472ea7e640b2ed95ed7507
VuyoYapi <103922433+VuyoYapi@users.noreply.github.com>
2024-09-11 13:20:17 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Bee Hive Care About</title>
+	  <link rel="icon" href="Images/logo12-removebg-preview.png" width="100px" height="145px">
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+    </head>
+    <body>
+      <!-- header section start -->
+      <div class="header_section">
+        <nav class="destop_header navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"></div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Traning1.php">Training</a>
+              </li>
+              <li class="nav-item">
+                <a class="logo_main" href="index.php"><img src="images/logo12-removebg-preview.png"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Wishlist.php">Wishlist</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Income-Generator.php">Income-Generater</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+			  </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Gallery.php">Gallery</a>
+              </li>
+			  </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Donations.php">Donations</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src="images/search-icon.png"></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="LoginPage.php">LOGIN</a>
+              </li>
+<li class="nav-item active">
+                <a class="nav-link" href="https://www.tshupehospice.co.za/payfastdonate.html">Donate</a>
+              </li> 	              
+            </ul>
+          </div>
+        </nav>
+        <nav class="mobile_header navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Training1.php">Training</a>
+              </li>
+              <li class="nav-item">
+                <a class="logo_main" href="index.php"><img src="images/logo.png"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Wishlist.php">Wishlist</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Incomer-Generator.php">Income-Generator</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="Gallery.php">Gallery</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="Donations.php">Donations</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src="images/search-icon.png"></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="LoginPage.php">LOGIN</a>
+              </li> 
+<li class="nav-item active">
+                <a class="nav-link" href="https://www.tshupehospice.co.za/payfastdonate.html">Donate</a>
+              </li> 			  
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <!-- header section end -->
+      <!-- about section start -->
+      <div class="about_section layout_padding">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="about_taital">
+                <h4 class="highest_text">About</h4>
+				
+                <h1 class="about_text">WHO WE ARE?</h1>
+                <p class="lorem_text">For over 20 years, Drs. Marc Aguirre and Mark Ottenweller have worked to assist HOPE worldwide’s HIV/AIDS programs in Africa which were started with the Abidjan HIV/AIDS Clinic in 1989. Since then community HIV/AIDS programs have been started in Kenya, Ivory Coast, South Africa, Botswana, Mozambique, Zambia, and Zimbabwe. These programs receive ongoing support from the HOPE worldwide Africa (HWW Africa) Team lead by Drs. Aguirre and Ottenweller.
+
+Initially, most programs began with HIV Prevention and Care and Support of Adults Infected with HIV. With the massive and growing orphan crisis, Care and Support programs for Orphans and Vulnerable Children (OVC) were established. Over the last 10 years those OVC programs have assisted over 150,000 needy children in Africa.</p>
+              
+				
+                <h1 class="about_text">DONOR AND PARTNER DEVELOPMENT</h1>
+                <p class="lorem_text">During 2005-2010, the unique ANCHOR Partnership between HOPE worldwide, Coca-Cola, Rotarians for Fighting AIDS, and the Emory School of Public Health allowed HOPE worldwide to scale up its OVC efforts to 7 countries. This partnership helped HWW Africa to develop partnerships with Coca Cola, its Business Units, and its Bottlers and Rotary Clubs around Africa. Partnerships have also been built with Shell Oil, AusAid, Massmart/Wal-Mart, KFC, Halliburton, CDC, and USAID, as well as a host of local community and government partners.</p>
+                
+				<div class="read_bt"><a href="#">Read More</a></div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="image_2" href="#"><img src="images/tshupenurse.jpg"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- about section end -->
+      <!-- footer section start -->
+	<div class="contact_section layout_padding">
+        <div class="container">
+       <div class="client_main">
+                      <div class="image_5"><img src="images/khrhghrae'ogho.png"></div>
+                      <p class="lorem_ipsum_text">The ability to bring aid into South-Africa as it's collaping before our eyes.
+
+Food, water, fuel and medicines are running out. Children's time is running out.
+
+With the entry of aid blocked, the intensifying fighting in Rafah, and no way out, children need action now!</p>
+                      <div class="quote_icon"><img src="images/quote-icon.png"></div>
+                    </div>
+        </div>
+      </div>
+      <!-- info section -->
+ <div class="info_section layout_padding">
+      <div class="container ">
+        <div class="info_content">
+              <div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Child Care
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                 
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Income-Generator
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+                  
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                 
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Training
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Gallery.php">
+                      Gallery
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center align-items-lg-baseline">
+          <div class="social-box">
+            <a href="">
+              <img src="images/fb-icon.png" alt="" />
+            </a>
+
+            <a href="">
+              <img src="images/twitter-icon.png" alt="" />
+            </a>
+            <a href="">
+              <img src="images/linkedin-icon.png" alt="" />
+            </a>
+            <a href="">
+              <img src="images/instagram-icon.png" alt="" />
+            </a>
+          </div>
+          <div class="form_container mt-5">
+            <form action="">
+              <label for="subscribeMail">
+                Newsletter
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Your email"
+                id="subscribeMail"
+              />
+              <button type="submit">
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <!-- end info section -->
+    <!-- footer section end -->
+    <!-- copyright section start -->
+    <div class="copyright_section">
+      <div class="container">
+        <p class="copyright">2024 All Rights Reserved. Design by <a href="https://html.design">Vuyo Yapi Designs</a></p>
+      </div>
+    </div>
+    <!-- copyright section end -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+   </body>
+   </html>
