af421c63e7752248614441f58865e6aa13e36273
VuyoYapi <103922433+VuyoYapi@users.noreply.github.com>
2024-09-11 13:11:19 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,452 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Bee-hive Care Homepage</title>
+	  <link rel="icon" href="Images/logo12-removebg-preview.png" width="100px" height="145px">
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+    </head>
+    <body>
+      <!-- header section start -->
+      <div class="header_section">
+        <nav class="destop_header navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"></div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Traning1.php">Training</a>
+              </li>
+              <li class="nav-item">
+                <a class="logo_main" href="index.php"><img src="images/logo12-removebg-preview.png"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Wishlist.php">Wishlist</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Incomer-Generator.php">Income-Generator</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="Gallery.php">Gallery</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="Donations.php">Donations</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src="images/search-icon.png"></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="LoginPage.php">LOGIN</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="https://www.tshupehospice.co.za/payfastdonate.html">Donate</a>
+              </li> 			  
+			  
+            </ul>
+          </div>
+        </nav>
+        <nav class="mobile_header navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="doctor.html">Doctor</a>
+              </li>
+              <li class="nav-item">
+                <a class="logo_main" href="index.html"><img src="images/logo.png"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="depatments.html">Depatments</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="blog.html">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src="images/search-icon.png"></a>
+              </li>
+              <li class="nav-item active">
+                <a class="read_bt" href="#">LOGIN</a>
+              </li>              
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <!-- header section end -->
+      <!-- banner section start -->
+    
+      <!-- banner section end -->
+      <!-- about section start -->
+     
+      <!-- about section end -->
+      <!-- care section start -->
+      <div class="care_section layout_padding">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="image_3" href="#"><img src="images/Children-Africa.JPG"></div>
+            </div>
+            <div class="col-md-6">
+              <div class="care_taital">
+                <h4 class="finest_text">Bee Hive Care</h4>
+                <h1 class="care_text">Company</h1>
+                <p class="ipsum_text">Mr and Ms Rangaka, founders of Tshupe Hospice (01/08/2001) and owners of Foro Super Spar in Tlhabane, became increasingly aware in the number of children foraging for food in and around the premises of their business, in attempt to contact family members and /or relatives suffering from HIV/AIDS or other terminal diseases such as Tuberculosis or Cancer.
+
+
+<div class="read_bt_2"><a href="#">Read More</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- care section end -->
+      <!-- services section start -->
+      <div class="services_section layout_padding">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="call_box">
+                <div class="call_image"><img src="images/call-icon.png"></div>
+                <h2 class="emergency_text">Emergency Cases</h2>
+                <h1 class="call_text">1-800-400-5768</h1>
+                <p class="dolor_text">Children’s lives, safety, well-being, and futures are under attack in an increasingly dangerous and unequal world.  </p>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="call_box active">
+                <div class="call_image"><img src="images/time-seat-icon.png"></div>
+                <h2 class="emergency_text">Children timetable</h2>
+                <h1 class="call_text">1-800-400-5768</h1>
+                <p class="dolor_text">Nearly 300 million people – or 1 in 27 people – are expected to need humanitarian assistance this year. We will aim to assist 15.9 million children across 46 countries in response to the most significant threat to children’s rights in recent memory. </p>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="call_box">
+                <div class="call_image"><img src="images/watch-icon.png"></div>
+                <h2 class="emergency_text">Opening hours</h2>
+                <h1 class="call_text">1-800-400-5768</h1>
+                <p class="dolor_text">Every day, our teams are saving lives in hard-to-reach communities – whether in war zones or remote villages. We train frontline health workers in communities to diagnose and treat common childhood illnesses like pneumonia, diarrhoea, malaria and newborn sepsis, empowering them to provide services that are proven to reduce child deaths. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- services section end -->
+      <!-- doctor section start -->
+      <div class="doctor_section layout_padding">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 padding_top0">
+              <h4 class="about_text">Saving lives</h4>
+                <h1 class="highest_text">Impact on the community</h1>
+                <p class="lorem_text">HWA has created effective, community-based models for Orphans and Vulnerable Children (OVC) which use a child-centered, family-focused, and community-based integrated approach. HWA’s model for community care and support through community mobilization has been recognized by UNAIDS as a “Best Practice” Model</p>
+                <div class="read_main">
+                  <div class="read_bt"><a href="#">Read More</a></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+              <div class="image_4"><img src="images/istockphoto-610850432-612x612-removebg-preview.png"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- doctor section end -->
+      <!-- contact section start -->
+      <div class="contact_section layout_padding">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h1 class="contact_text">Contact Us</h1>
+                <form action="/action_page.php">
+                  <div class="form-group">
+                    <input type="text" class="email-bt" placeholder="Name" name="Name">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="email-bt" placeholder="Email" name="Email">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="email-bt" placeholder="Subject" name="Email">
+                  </div>
+                    <div class="form-group">
+                      <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="text"></textarea>
+                    </div>
+                  </form>
+                  <div class="main_bt"><a href="contact.php">Send</a></div>
+              </div>
+            <div class="col-md-6">
+              <h1 class="contact_text">Who are our employee</h1>
+              <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <h2 class="selfideno_text">The helping Program</h2>
+                    <div class="client_main">
+                      <div class="image_5"><img src="images/1517614039848fduyf.jpg"></div>
+                      <p class="lorem_ipsum_text">The ability to bring aid into South-Africa as it's collaping before our eyes.
+
+Food, water, fuel and medicines are running out. Children's time is running out.
+
+With the entry of aid blocked, the intensifying fighting in Rafah, and no way out, children need action now!
+
+Please donate today to protect children's lives.</p>
+                      <div class="quote_icon"><img src="images/quote-icon.png"></div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <h2 class="selfideno_text">The helping Program</h2>
+                    <div class="client_main">
+                      <div class="image_5"><img src="images/testimonial-lihle.png"></div>
+                      <p class="lorem_ipsum_text">The ability to bring aid into South-Africa as it's collaping before our eyes.
+
+Food, water, fuel and medicines are running out. Children's time is running out.
+
+With the entry of aid blocked, the intensifying fighting in Rafah, and no way out, children need action now!</p>
+                      <div class="quote_icon"><img src="images/quote-icon.png"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- contact section end -->
+      <!-- appointment section start -->
+      <div class="appointment_section">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h3 class="need_text">Want to help the children </h3>
+              <h1 class="make_text">You can donate a little</h1>
+            </div>
+            <div class="col-md-6">
+              <div class="appointment_bt_main">
+                <div class="appointment_bt"><a href="https://www.tshupehospice.co.za/payfastdonate.html">Donate</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- appointment section end -->
+      <!-- footer section start -->
+      <!-- info section -->
+    <div class="info_section layout_padding">
+      <div class="container ">
+        <div class="info_content">
+              <div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Child Care
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                 
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Income-Generator
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+                  
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                 
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="d-flex">
+                <h5>
+                  Training
+                </h5>
+              </div>
+              <div class="d-flex ">
+                <ul>
+                  <li>
+                    <a href="about.php">
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Wishlist.php">
+                      Wishlist
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Traning1.php">
+                      Training
+                    </a>
+                  </li>
+                  <li>
+                    <a href="Gallery.php">
+                      Gallery
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.php">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+                <ul class="ml-3 ml-md-5">
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center align-items-lg-baseline">
+          <div class="social-box">
+            <a href="https://www.facebook.com/profile.php?id=100081859935495&mibextid=LQQJ4d">
+              <img src="images/fb-icon.png" alt="" />
+            </a>
+
+            <a href="https://x.com/danielmantji27?s=21">
+              <img src="images/twitter-icon.png" alt="" />
+            </a>
+            <a href="https://www.linkedin.com/in/vuyo-yapi-19a00b251?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+              <img src="images/linkedin-icon.png" alt="" />
+            </a>
+            <a href="https://www.instagram.com/daniel_mantji?igsh=bWl4ZTN4bGNycnRr&utm_source=qr">
+              <img src="images/instagram-icon.png" alt="" />
+            </a>
+          </div>
+          <div class="form_container mt-5">
+            <form action="">
+              <label for="subscribeMail">
+                Newsletter
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Your email"
+                id="subscribeMail"
+              />
+              <button type="submit">
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <!-- end info section -->
+    <!-- footer section end -->
+    <!-- copyright section start -->
+    <div class="copyright_section">
+      <div class="container">
+        <p class="copyright">2024 All Rights Reserved. Design by <a href="https://html.design">Vuyo Yapi Designs</a></p>
+      </div>
+    </div>
+    <!-- copyright section end -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+   </body>
+   </html>
